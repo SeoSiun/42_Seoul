9f99b825ea288d5feb894e807cd55933fcf33091
SeoSiun <siun0816@hanyang.ac.kr>
2022-06-26 01:10:06 +0900

    timeout이 strlen 때문인가 해서 strlen 없애려고 구조체 만들고 생난리 쳤는데 안됨;; 그리고 free_backup에서 segfault 나는데 이유를 모르겠음.

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: siseo <siseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 15:18:13 by siseo             #+#    #+#             */
-/*   Updated: 2022/06/26 00:09:52 by siseo            ###   ########.fr       */
+/*   Updated: 2022/06/26 00:31:24 by siseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 char	*get_next_line(int fd)
 {
-	char		buf[BUFFER_SIZE + 1];
-	static char	*backup[OPEN_MAX];
-	char		*line;
-	int			size;
-	size_t		b_len;
+	char			buf[BUFFER_SIZE + 1];
+	static t_backup	backup[OPEN_MAX];
+	char			*line;
+	int				size;
 
 	line = 0;
 	while (1)
@@ -28,24 +27,14 @@ char	*get_next_line(int fd)
 			return (free_backup(&backup[fd]));
 		if (size == 0)
 		{
-			check_new_line(&backup[fd], &line, size, 0);
+			check_new_line(&backup[fd], &line, size);
 			return (line);
 		}
 		buf[size] = '\0';
-		backup[fd] = ft_strjoin(backup[fd], buf, size, &b_len);
-		if (!backup[fd])
+		backup[fd].s = ft_strjoin(&backup[fd], buf, size);
+		if (!backup[fd].s)
 			return (0);
-		if (check_new_line(&backup[fd], &line, size, b_len))
+		if (check_new_line(&backup[fd], &line, size))
 			return (line);
 	}
-}
-
-char	*free_backup(char **backup)
-{
-	if (*backup)
-	{
-		free(*backup);
-		*backup = 0;
-	}
-	return (0);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,23 +6,21 @@
 /*   By: siseo <siseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 15:18:19 by siseo             #+#    #+#             */
-/*   Updated: 2022/06/25 23:53:30 by siseo            ###   ########.fr       */
+/*   Updated: 2022/06/26 00:46:13 by siseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+char	*free_backup(t_backup *backup)
 {
-	size_t	len;
-
-	len = 0;
-	while (*s)
+	if (backup->s)
 	{
-		s++;
-		len++;
+		free(backup->s);
+		backup->s = 0;
+		backup->len = 0;
 	}
-	return (len);
+	return (0);
 }
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dst_len, size_t dstsize)
@@ -43,36 +41,33 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dst_len, size_t dstsize)
 	return (dstsize + dstsize - dst_len - 1);
 }
 
-char	*ft_strjoin(char *s1, char *s2, size_t s2_size, size_t *b_len)
+char	*ft_strjoin(t_backup *backup, char *s2, size_t s2_size)
 {
-	size_t	s1_size;
 	char	*result;
 
-	if (!s1)
+	if (!backup->s)
 	{
-		s1 = malloc(sizeof(char));
-		s1[0] = '\0';
+		backup->s = malloc(sizeof(char));
+		backup->s[0] = '\0';
 	}
-	s1_size = ft_strlen(s1);
-	result = malloc(sizeof(char) * (s1_size + s2_size + 1));
+	result = malloc(sizeof(char) * (backup->len + s2_size + 1));
 	if (!result)
 		return (0);
 	result[0] = '\0';
-	ft_strlcat(result, s1, 0, s1_size + 1);
-	ft_strlcat(result, s2, s1_size, s1_size + s2_size + 1);
-	free(s1);
-	*b_len = s1_size + s2_size + 1;
+	ft_strlcat(result, backup->s, 0, backup->len + 1);
+	ft_strlcat(result, s2, backup->len, backup->len + s2_size + 1);
+	free(backup->s);
+	backup->len = backup->len + s2_size;
 	return (result);
 }
 
-size_t	ft_strlcpy(char **dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(t_backup *backup, const char *src, size_t dstsize)
 {
 	size_t	i;
 
 	if (*src == '\0')
 	{
-		free(*dst);
-		*dst = 0;
+		free_backup(backup);
 		return (0);
 	}
 	else if (dstsize != 0)
@@ -80,38 +75,34 @@ size_t	ft_strlcpy(char **dst, const char *src, size_t dstsize)
 		i = 0;
 		while (i < dstsize - 1 && src[i] != '\0')
 		{
-			(*dst)[i] = src[i];
+			backup->s[i] = src[i];
 			i++;
 		}
-		(*dst)[i] = '\0';
+		backup->s[i] = '\0';
+		backup->len = i;
 	}
 	return (dstsize - 1);
 }
 
-int	check_new_line(char **backup, char **line, int size, size_t b_len)
+int	check_new_line(t_backup *backup, char **line, int size)
 {
 	int		i;
 
 	if (line && *line)
 		free(*line);
-	if (!*backup)
+	if (!backup->s)
 		return (0);
-	if (b_len == 0)
-		b_len = ft_strlen(*backup);
-	*line = malloc(sizeof(char) * (b_len + 1));
+	*line = malloc(sizeof(char) * (backup->len + 1));
 	i = -1;
-	while ((*backup)[++i] != '\0' && (i == 0 || (*backup)[i - 1] != '\n'))
-		(*line)[i] = (*backup)[i];
+	while (backup->s[++i] != '\0' && (i == 0 || backup->s[i - 1] != '\n'))
+		(*line)[i] = backup->s[i];
 	(*line)[i] = '\0';
-	if ((*backup)[i - 1] == '\n')
+	if (i != 0 && backup->s[i - 1] == '\n')
 	{
-		ft_strlcpy(backup, &((*backup)[i]), b_len - (i - 1) + 1);
+		ft_strlcpy(backup, &(backup->s[i]), backup->len - (i - 1) + 1);
 		return (1);
 	}
-	else if (size == 0 && (*backup)[i] == '\0')
-	{
-		free(*backup);
-		*backup = 0;
-	}
+	else if (size == 0 && backup->s[i] == '\0')
+		free_backup(backup);
 	return (0);
 }
